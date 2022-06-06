3b5865d9f0accdc4a53ea0a0892ded6468d362dd
Reece Hart <reecehart@gmail.com>
2022-06-05 17:58:36 -0700

    exclude shortcut from requests_cache

--- src/shortcut_importer/__main__.py
+++ src/shortcut_importer/__main__.py
@@ -24,11 +24,18 @@ requests_cache.install_cache(
     config["requests_cache_filename"],
     backend="sqlite",
     expire_after=config["requests_cache_ttl"],
+    urls_expire_after={
+        "api.app.shortcut.com": requests_cache.DO_NOT_CACHE
+    }
 )
 _logger.info(
     "Installed requests cache %s w/%d s TTL"
     % (config["requests_cache_filename"], config["requests_cache_ttl"])
 )
+
+cache = requests_cache.get_cache()
+if any(u for u in cache.urls if "shortcut" in u):
+    raise RuntimeError("cache contains shortcut URLs which fails when using multiple workspaces")
 
 impr = Importer(config)
 
